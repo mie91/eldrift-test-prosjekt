a2957227bf549656857feed3f6f4ebb9064b50ad
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-11-17 18:13:24 +0100

    Update Toolbar.vue

--- src/components/layout/Toolbar.vue
+++ src/components/layout/Toolbar.vue
@@ -28,14 +28,14 @@
   </v-app-bar>
   <v-navigation-drawer app v-model="drawer" class="elblue">
 <v-list>
-    <v-list-tile>
-        <v-list-tile-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
-        </v-list-tile-content>
-    </v-list-tile>
+    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+        <v-list-item-action>
+            <v-icon color="elblack" >{{ link.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+        </v-list-item-content>
+    </v-list-item>
 </v-list>
   </v-navigation-drawer>
   </nav>
@@ -46,7 +46,12 @@ export default {
   name: "Toolbar",
     data () {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+                { icon: 'mdi-account', text: 'Team', route: '/team' },
+            ]
         }
     }
 };
