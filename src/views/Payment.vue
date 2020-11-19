ad9def89154a20a38fc8cafb97952abc38e9ad8c
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-11-19 08:34:08 +0100

    Update Payment.vue

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,14 +1,124 @@
 <template>
-  <div class="payment">
-    <h1 class="elblue--text">Payment</h1>
-    <v-container class="my-5">
-      content
+  <div class="payments">
+    <v-container my-5>
+      <h1 class="elblue--text">Payments</h1>
+      
+      <v-row flex class="ma-2 ">
+        <v-btn  small flat class="mr-1" @click="sortBy('amount')">
+          <v-icon left >mdi-cash</v-icon>
+          <span>By amount</span>
+        </v-btn>
+        <v-btn small flat class="ml-1" @click="sortBy('person')">
+          <v-icon left>mdi-account</v-icon>
+          <span>By person</span>
+        </v-btn>
+      </v-row>
+
+      
+      <v-card
+        tile
+        class="px-3 ma-1"
+        v-for="payment in payments"
+        :key="payment.name"
+      >
+        <v-row :class="`pl-3 payment ${payment.status}`">
+          
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption elblue--text">Person</div>
+            <div>{{ payment.person }}</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption elblue--text">Due By</div>
+            <div>{{ payment.due }}</div>
+          </v-col>
+          <v-col cols="12" md="6">
+            <div class="caption elblue--text">Amount</div>
+            <div>{{ payment.amount }} kr</div>
+          </v-col>
+          <v-col cols="6" sm="2" md="2">
+            <div>
+                <div class="caption elblue--text">Staus</div>
+              <v-chip :class="`${payment.status}`">
+                {{ payment.status }}
+              </v-chip>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      payments: [
+        {
+          amount: 1200,
+          person: "Marie",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          amount: 1340,
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          amount: 987,
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          amount: 1421,
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        }
+      ]
+    };
+  },
+  methods: {
+    sortBy(prop) {
+      this.payments.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
+    }
+  }
+};
 </script>
 
-<style></style>
+<style lang="scss">
+.payment.complete {
+  border-left: 4px solid $elgreen;
+}
+
+.payment.ongoing {
+  border-left: 4px solid $elblue;
+}
+
+.payment.overdue {
+  border-left: 4px solid $elyellow;
+}
+
+.theme--light.v-chip.complete:not(.v-chip--active) {
+  background: $elgreen;
+}
+
+.theme--light.v-chip.ongoing:not(.v-chip--active) {
+  background: $elblue;
+}
+
+.theme--light.v-chip.overdue:not(.v-chip--active) {
+  background: $elyellow;
+}
+</style>
